7f509bc3dcf85e264ed15ade39f5d4231fb50b6e
Harsh Joshi <joshih1998@gmail.com>
2019-06-02 14:20:54 +0530

    Updates

---
+++ precache-manifest.2c8898da94f66dc58ac7d825a7ac719d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3516ac1f295995236dbb422aa7f529a8",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "1a1852c412281427ce42",
+    "url": "/mycv/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "757a0729724b8a8d048a",
+    "url": "/mycv/static/css/main.5fa7a017.chunk.css"
+  },
+  {
+    "revision": "1a1852c412281427ce42",
+    "url": "/mycv/static/js/2.cd057a66.chunk.js"
+  },
+  {
+    "revision": "757a0729724b8a8d048a",
+    "url": "/mycv/static/js/main.b9392177.chunk.js"
+  },
+  {
+    "revision": "c4cf67da64f3e9288cb6",
+    "url": "/mycv/static/js/runtime~main.ea5db7c0.js"
+  },
+  {
+    "revision": "95e20e3e84047a65108190404e40c347",
+    "url": "/mycv/static/media/SP.95e20e3e.jpg"
+  },
+  {
+    "revision": "f7c526926ff1f5f8d8fb3ea0ee7f82ae",
+    "url": "/mycv/static/media/logo.f7c52692.jpg"
+  }
+]);
